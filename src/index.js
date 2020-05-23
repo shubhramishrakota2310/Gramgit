483120e8e52c43df583098926c8a4286ecb9d573
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-05-24 00:10:21 +0530

    deploy my react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './main.css'
+class Application extends React.Component {
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    constructor(props) {
+      super(props);
+      this.state = {
+        data:[],
+      }
+    }
+    
+    fileChangedHandler = event => {
+        this.setState({
+          selectedFile: event.target.files[0]
+        })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        let reader = new FileReader();
+     
+        reader.onloadend = () => {
+          this.setState({
+            imagePreviewUrl: reader.result
+          });
+        }
+     
+        reader.readAsDataURL(event.target.files[0])
+     
+      }   
+    onSubmit = event => {
+      event.preventDefault();
+      const username = this.username.value;
+      const caption = this.caption.value;
+      const imagePreviewUrl = this.imagePreviewUrl.value;
+      const info = {username: username, caption: caption, imagePreviewUrl:<img src={this.state.imagePreviewUrl} alt="icon" width="600" />  };
+      const data = [...this.state.data, info];
+      this.setState({
+        data: data
+      });
+    };
+  
+    render() {
+        let $imagePreview = (<div className="previewText image-container"></div>);
+    if (this.state.imagePreviewUrl) {
+      $imagePreview = (<div className="image-container" ><img src={this.state.imagePreviewUrl} alt="icon" width="500" /> </div>);
+    }
+      return (
+          <div className="container">
+            <h1>Post your images with a quirky caption!</h1>
+  
+            <hr/>
+  
+            <div className="row">
+              <form className="form-inline" onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    className="form-control mb-2 mr-sm-2 mb-sm-0"
+                    placeholder="Username"
+                    ref={input => this.username = input}/>
+                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                  <input
+                      type="text"
+                      className="form-control"
+                      placeholder="caption"
+                      ref={input => this.caption = input}/>
+                </div>
+                <input
+                      type="file"
+                      className="form-control"
+                      placeholder="post"
+                      onChange={this.fileChangedHandler}
+                      ref={input => this.imagePreviewUrl = input}/>
+                      
+                <button type="submit" className="btn btn-primary">Save</button>
+                
+              </form>
+            </div>
+  
+            <hr/>
+  
+            <div className="row">
+              {
+                this.state.data.map((info, index) => <Card key={index} info={info}/>)
+              }
+            </div>
+  
+          </div>
+      )
+    }
+  }
+  
+  const Card = props =>
+      <div className="col-md-6 col-lg-3">
+        <div className="card mb-3">
+          <div className="card-body">
+            <p className="card-title"><span>Username: </span>{props.info.username} </p>
+            <p className="card-text">
+              <span> </span>{props.info.imagePreviewUrl}
+            <p className="card-post">
+              <span>caption: </span>{props.info.caption}</p>
+              
+            </p>
+          </div>
+        </div>
+      </div>;
+  
+  ReactDOM.render(
+      <Application/>,
+      document.getElementById('root')
+  );
