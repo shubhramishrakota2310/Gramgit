9ef9ac88e9211df9cb09049f960ca7c3d07964e7
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-05-24 00:11:51 +0530

    Updates

---
+++ precache-manifest.70b8f00f32cb8ef070905d0ee09ba3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb01f614144c2c3c5d04b276ac0f55af",
+    "url": "/Gramgit/index.html"
+  },
+  {
+    "revision": "ccfaf0f5f2771df03b20",
+    "url": "/Gramgit/static/css/main.6e6edea8.chunk.css"
+  },
+  {
+    "revision": "e1066dd454950b603543",
+    "url": "/Gramgit/static/js/2.69ebc455.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gramgit/static/js/2.69ebc455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfaf0f5f2771df03b20",
+    "url": "/Gramgit/static/js/main.a0d6bc5e.chunk.js"
+  },
+  {
+    "revision": "8b072351534a7c0be2e9",
+    "url": "/Gramgit/static/js/runtime-main.b54b46f7.js"
+  }
+]);
