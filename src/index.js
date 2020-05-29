61235bfd258ee701412cdb25fcdec4c7f45290f2
shubhra2310 <shubhramishrakota2018@gmail.com>
2020-05-29 12:50:56 +0530

    controlled input

--- src/index.js
+++ src/index.js
@@ -6,102 +6,106 @@ class Application extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        data:[],
+        file: null,
+        name:'',
+        caption:'',
+        data:[]
       }
+      this.handleChange = this.handleChange.bind(this)
     }
     
-    fileChangedHandler = event => {
-        this.setState({
-          selectedFile: event.target.files[0]
-        })
+    handleChange(event) {
+      this.setState({
+        file: URL.createObjectURL(event.target.files[0])
+      })
+    }
 
-        let reader = new FileReader();
-     
-        reader.onloadend = () => {
-          this.setState({
-            imagePreviewUrl: reader.result
-          });
-        }
-     
-        reader.readAsDataURL(event.target.files[0])
-     
-      }   
-    onSubmit = event => {
+    handleNameChange = (event) => {
+      this.setState({ name: event.target.value});
+    };
+
+    handleCaptionChange = (event) => {
+      this.setState({ caption: event.target.value});
+    };
+
+    handleSubmit = event => {
       event.preventDefault();
-      const username = this.username.value;
-      const caption = this.caption.value;
-      const imagePreviewUrl = this.imagePreviewUrl.value;
-      const info = {username: username, caption: caption, imagePreviewUrl:<img src={this.state.imagePreviewUrl} alt="icon" width="600" />  };
+      const info = {name: this.state.name, caption: this.state.caption, file:<img src={this.state.file} alt="icon" width="600" />}
       const data = [...this.state.data, info];
       this.setState({
         data: data
       });
     };
+   
   
+    
+      
     render() {
-        let $imagePreview = (<div className="previewText image-container"></div>);
-    if (this.state.imagePreviewUrl) {
-      $imagePreview = (<div className="image-container" ><img src={this.state.imagePreviewUrl} alt="icon" width="500" /> </div>);
-    }
+       
       return (
-          <div className="container">
+          <div className="container" onSubmit={this.handleSubmit}>
             <h1>Post your images with a quirky caption!</h1>
   
             <hr/>
   
             <div className="row">
-              <form className="form-inline" onSubmit={this.onSubmit}>
+              <form className="form-inline" layout="inline">
+              <div className="input-group mb-2 mr-sm-2 mb-sm-0">
                 <input
                     type="text"
                     className="form-control mb-2 mr-sm-2 mb-sm-0"
-                    placeholder="Username"
-                    ref={input => this.username = input}/>
-                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                    value={this.state.name}
+                    onChange={this.handleNameChange}/>
+                
                   <input
                       type="text"
                       className="form-control"
                       placeholder="caption"
-                      ref={input => this.caption = input}/>
-                </div>
+                      value={this.state.caption}
+                      onChange={this.handleCaptionChange}/>
+              
+                
                 <input
                       type="file"
                       className="form-control"
                       placeholder="post"
-                      onChange={this.fileChangedHandler}
-                      ref={input => this.imagePreviewUrl = input}/>
+                      onChange={this.handleChange}/>
+                      
+                  </div>
                       
                 <button type="submit" className="btn btn-primary">Save</button>
                 
               </form>
             </div>
-  
-            <hr/>
-  
+
             <div className="row">
               {
                 this.state.data.map((info, index) => <Card key={index} info={info}/>)
               }
             </div>
   
+            
+  
           </div>
       )
     }
   }
-  
+
+
   const Card = props =>
-      <div className="col-md-6 col-lg-3">
-        <div className="card mb-3">
-          <div className="card-body">
-            <p className="card-title"><span>Username: </span>{props.info.username} </p>
-            <p className="card-text">
-              <span> </span>{props.info.imagePreviewUrl}
-            <p className="card-post">
-              <span>caption: </span>{props.info.caption}</p>
-              
-            </p>
-          </div>
-        </div>
-      </div>;
+  <div className="col-md-6 col-lg-3">
+    <div className="card mb-3">
+      <div className="card-body">
+        <p className="card-title"><span>Username: </span>{props.info.name} </p>
+        <p className="card-text">
+          <span> </span>{props.info.file}
+        <p className="card-post">
+          <span>caption: </span>{props.info.caption}</p>
+          
+        </p>
+      </div>
+    </div>
+  </div>;
   
   ReactDOM.render(
       <Application/>,
